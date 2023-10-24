0211370e1d6c6d0a01ca302644fcfc277dbc9db1
franken <franken1817@gmail.com>
2023-10-24 14:35:56 +0800

    Updates

---
+++ 375.e2d30c063347db3aae48.js
@@ -0,0 +1,103 @@
+"use strict";(self.webpackChunkfrankenworks_github_io=self.webpackChunkfrankenworks_github_io||[]).push([[375],{375:(e,t,n)=>{n.r(t),n.d(t,{default:()=>U});var l=n(294),a=n(394),r=n(250),i=n(846),c=n(655);const o=a.ZP.div`
+  display: flex;
+  flex-direction: column;
+`,m=a.ZP.div`
+  display: grid;
+  grid-template-columns: auto;
+  align-items: flex-start;
+  gap: 0.5rem;
+`,s=(0,a.ZP)(c.OL)`
+  &:link,
+  &:visited {
+    display: flex;
+    flex-direction: column;
+    padding: 0.25rem 1rem;
+    border: 1px solid black;
+    transition: all 0.3s;
+    border-radius: 9px;
+    text-decoration: none;
+  }
+
+  &:hover {
+    border: 1px solid #ffa500;
+    color: #ffa500;
+  }
+  ,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    border: 1px solid #ffa500;
+    color: #ffa500;
+    text-decoration: none;
+  }
+`,d=a.ZP.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%
+  gap: 5rem;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 59em) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`,u=function(){return l.createElement(o,null,l.createElement("div",null,l.createElement(i.Z,{as:"h5"},"Experience")),l.createElement(s,{to:"/experience/wk-2"},l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Consultant")),l.createElement(d,null,l.createElement("p",null,"Innovative Investors Finance Corporation"),l.createElement("p",null,"2023"))),l.createElement(m,null,l.createElement(s,{to:"/experience/wk-1"},l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Web Developer")),l.createElement(d,null,l.createElement("p",null,"Freelance"),l.createElement("p",null,"2022 to 2023")))),l.createElement("div",null,l.createElement(i.Z,{as:"h5"},"Education")),l.createElement(m,null,l.createElement(s,{to:"/experience/ed-3"},l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Coursework towards Masters")),l.createElement(d,null,l.createElement("p",null,"MS in Computer Science at UP Diliman"),l.createElement("p",null,"2019 to 2022"))),l.createElement(s,{to:"/experience/ed-2"},l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Undergraduate")),l.createElement(d,null,l.createElement("p",null,"BS Mechanical Engineering at UP Diliman"),l.createElement("p",null,"2012 to 2018"))),l.createElement(s,{to:"/experience/ed-1"},l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Secondary")),l.createElement(d,null,l.createElement("p",null,"Philippine Science High School - Main Campus"),l.createElement("p",null,"2009 to 2012")))))},p=n.p+"0e7f0647c57f33d974fba6a04ca03599.png",h=a.ZP.header`
+  display: flex;
+  gap: 2rem;
+`,E=a.ZP.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  gap: 2rem;
+`,g=a.ZP.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`,f=a.ZP.p`
+  @media (max-width: 59em) {
+    font-size: 1.25rem;
+  }
+`,v=(a.ZP.div`
+  background-color: #fff;
+`,a.ZP.img`
+  background-color: #fff;
+`),y=function(){return l.createElement(E,null,l.createElement(h,null,l.createElement(v,{height:"80rem",src:p,alt:"PSHS Logo"}),l.createElement(g,null,l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Philippine Science High School")),l.createElement("div",null,l.createElement(i.Z,{as:"h3"},"Main Campus")))),l.createElement(f,null,"Philippine Science High School is a research-oriented and specialized public high school. Operating under the DOST, only 240 students enter the main campus branch every year out of thousands that apply from all over the country."),l.createElement(f,null,"Here, I was introduced to programming, where I learned C++, Java and Python. In my 4th year, I developed an Android application for the interface of an ECG device running on an Arduino microcontroller. The project won 2nd place in an international competition in Taiwan."))},w=a.ZP.div`
+  align-self: center;
+  justify-self: center;
+`,x=function(){return l.createElement(w,null,l.createElement(i.Z,{as:"h2"},"Page does not exist!"))},b=n.p+"2cbb64656e4d63355158e92f8cd75394.png",Z=(0,a.ZP)(c.rU)`
+  &:link,
+  &:visited {
+    display: inline-block;
+    transition: all 0.3s;
+    text-decoration: none;
+  }
+
+  &:hover {
+    color: #ffa500;
+  }
+  ,
+  &:active,
+  &.active:link,
+  &.active:visited {
+    text-decoration: none;
+  }
+`,k=function(){return l.createElement(E,null,l.createElement(h,null,l.createElement("img",{height:"80rem",src:b,alt:"UP Logo"}),l.createElement(h,null,l.createElement(g,null,l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"BS Mechanical Engineering")),l.createElement("div",null,l.createElement(i.Z,{as:"h3"},"University of the Philippines - Diliman"))))),l.createElement(f,null,"UP is among the top universities in the country and has consistently ranked among the top 400 universities in the"," ",l.createElement(Z,{to:"https://www.topuniversities.com/universities/university-philippines",target:"_blank"},"QS World University Ranking"),". My coursework was very diversified and I was exposed to many fields related to internal combustion engines, heating ventilation and air conditioning, control systems, electrical engineering, machine design and power plant engineering. This is mainly because the course is geared towards giving a comprehensive education in preparation for the board exam."),l.createElement(f,null,"I passed the board exam for Mechanical Engineering in 2018."))},P=function(){return l.createElement(E,null,l.createElement(h,null,l.createElement("img",{height:"80rem",src:b,alt:"UP Logo"}),l.createElement(h,null,l.createElement(g,null,l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"MS Computer Science")),l.createElement("div",null,l.createElement(i.Z,{as:"h3"},"University of the Philippines - Diliman"))))),l.createElement(f,null,"In 2019 I decided to enroll in a Masters Course for Computer Science. The pandemic had caused a delay in completing my studies, but by the end of 2022 I had completed all of my coursework except for the thesis requirement. I am still on leave status with the university, however I put it on hold as I wanted to focus more on my work in software development and data science."))};var I=n(225);const S=function(){return l.createElement(E,null,l.createElement(h,null,l.createElement(I.xn5,{size:"8rem"}),l.createElement(g,null,l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Consultant")),l.createElement("div",null,l.createElement(i.Z,{as:"h3"},"Innovative Investors and Financing Company")))),l.createElement(f,null,"I was mentored in data science where I was taught to use data analysis tools such as Microsoft Excel, as well as system design for database management systems(DBMS). I helped design and implement various systems for the company, such as a QR attendance system and a rental and leasing tracking system. My programming background also occassionally becomes useful for data cleanup and process automation using Python scripts."))};var C=n(516);const M=function(){return l.createElement(E,null,l.createElement(h,null,l.createElement(C.EYS,{size:"8rem"}),l.createElement(g,null,l.createElement("div",null,l.createElement(i.Z,{as:"h2"},"Web Developer")),l.createElement("div",null,l.createElement(i.Z,{as:"h3"},"Freelance")))),l.createElement(f,null,"Although I had first learned programming in 2009, I never really continued my learning in the field during my undergraduate studies. I self-studied web development in my spare time and learned HTML, CSS, Javascript and React. I did a few projects for static webpages, including this site."))},A=a.ZP.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: space-between;
+  grid-row-gap: 2rem;
+  height: 80vh;
+  @media (max-width: 75em) {
+    // font-size: 6rem;
+  }
+  @media (max-width: 59em) {
+    // font-size: 5rem;
+  }
+  @media (max-width: 44em) {
+    grid-template-columns: 1fr;
+  }
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    height: auto;
+  }
+`,U=function(){return l.createElement(A,null,l.createElement(u,null),l.createElement(r.Z5,null,l.createElement(r.AW,{index:!0,element:l.createElement(r.Fg,{replace:!0,to:"/experience/wk-2"})}),l.createElement(r.AW,{path:"/ed-1",element:l.createElement(y,null)}),l.createElement(r.AW,{path:"/ed-2",element:l.createElement(k,null)}),l.createElement(r.AW,{path:"/ed-3",element:l.createElement(P,null)}),l.createElement(r.AW,{path:"/wk-1",element:l.createElement(M,null)}),l.createElement(r.AW,{path:"/wk-2",element:l.createElement(S,null)}),l.createElement(r.AW,{path:"/*",element:l.createElement(x,null)})))}}}]);
