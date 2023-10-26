578f27511c36225bfc9dadd9195ccc187deea28e
franken <franken1817@gmail.com>
2023-10-26 14:47:58 +0800

    Updates

---
+++ 761.464e2346453994373736.js
@@ -0,0 +1,48 @@
+"use strict";(self.webpackChunkfrankenworks_github_io=self.webpackChunkfrankenworks_github_io||[]).push([[761],{761:(e,t,n)=>{n.r(t),n.d(t,{default:()=>d});var i=n(294),a=n(394),r=n(846);const m=n.p+"702a6d509ceea49640af9d3d6d49db79.png",l=a.ZP.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: space-between;
+  grid-column-gap: 6rem;
+  height: 90vh;
+
+  @media (max-height: 40em) {
+    height: auto;
+  }
+
+  @media (max-width: 59em) {
+    grid-template-columns: 1fr;
+  }
+`,o=a.ZP.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  // height: 50%;
+`,s=a.ZP.p`
+  max-width: 50rem;
+  @media (max-width: 75em) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 59em) {
+    width: 50%;
+    font-size: 1rem;
+  }
+  @media (max-width: 44em) {
+    font-size: 0.8rem;
+  }
+  @media (max-width: 34em) {
+    font-size: 0.8rem;
+  }
+`,h=a.ZP.img`
+  width: 80%;
+  max-width: 70rem;
+
+  -webkit-filter: brightness(120%) saturate(130%);
+  -moz-filter: brightness(120%) saturate(130%);
+  filter: brightness(120%) saturate(130%);
+
+  @media (max-width: 59em) {
+    max-width: 40rem;
+  }
+`,d=function(){return i.createElement(l,null,i.createElement(o,null,i.createElement(r.Z,{as:"h1"},"Who am I?"),i.createElement(r.Z,{as:"h2"},"To thine own self be true..."),i.createElement(s,null,"I have been through many engineering disciplines and have always found myself curious. But I'm really just a guy who loves math and computers."),i.createElement(r.Z,{as:"h2"},"Brevity is the soul of wit!"),i.createElement(s,null,"I am a man of few words, but by choice. Communication is very important to me, and if I can get people to understand in less words, the better."),i.createElement(r.Z,{as:"h2"},"There is no wealth like knowledge,",i.createElement("br",null)," no poverty like ignorance...."),i.createElement(s,null,"Every man dies, but not every man really lives. When you die, you're going to regret the things you don't do. When I die, I'm going to regret the things I don't know.")),i.createElement(o,null,i.createElement(h,{src:m,alt:"kenji on a ferris wheel"})))}}}]);
