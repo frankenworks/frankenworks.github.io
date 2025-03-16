17a4f60777b746aeb3186de47910854ec0b779f4
franken <franken1817@gmail.com>
2025-03-17 07:10:38 +0800

    Updates

---
+++ 904.6483611dff001bbec84a.js
@@ -0,0 +1,48 @@
+"use strict";(self.webpackChunkfrankenworks_github_io=self.webpackChunkfrankenworks_github_io||[]).push([[904],{904:(e,t,n)=>{n.r(t),n.d(t,{default:()=>u});var i=n(294),a=n(394),r=n(846);const l=n.p+"490f04ebdb3718fec005252f25db93e9.jpg",s=a.ZP.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: space-between;
+  grid-column-gap: 6rem;
+  height: 90vh;
+
+  @media (max-height: 40em) {
+    height: auto;
+  }
+
+  @media (max-width: 59em) {
+    grid-template-columns: 1fr;
+  }
+`,m=a.ZP.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  // height: 50%;
+`,o=a.ZP.p`
+  max-width: 50rem;
+  @media (max-width: 75em) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 59em) {
+    width: 50%;
+    font-size: 1rem;
+  }
+  @media (max-width: 44em) {
+    font-size: 0.8rem;
+  }
+  @media (max-width: 34em) {
+    font-size: 0.8rem;
+  }
+`,h=a.ZP.img`
+  width: 80%;
+  max-width: 70rem;
+
+  -webkit-filter: brightness(120%) saturate(130%);
+  -moz-filter: brightness(120%) saturate(130%);
+  filter: brightness(120%) saturate(130%);
+
+  @media (max-width: 59em) {
+    max-width: 40rem;
+  }
+`,u=function(){return i.createElement(s,null,i.createElement(m,null,i.createElement(r.Z,{as:"h4"},"Who am I?"),i.createElement(r.Z,{as:"h2"},"To thine own self be true..."),i.createElement(o,null,"I have been through many engineering disciplines and have always found myself curious. But I'm really just a guy who loves math and computers."),i.createElement(r.Z,{as:"h2"},"Brevity is the soul of wit!"),i.createElement(o,null,"People often describe me as quiet. But there is a difference between someone who has nothing to say and someone who is restrained. Communication is the key to any relationship."),i.createElement(r.Z,{as:"h2"},"There is no wealth like knowledge,",i.createElement("br",null)," no poverty like ignorance..."),i.createElement(o,null,"The moment you stop learning is the moment you stop growing. I have lost countless hours of sleep to studying, but I have not once regretted it. There is always something to learn, especially in failure.")),i.createElement(m,null,i.createElement(h,{src:l,loading:"lazy",alt:"kenji on a ferris wheel"})))}}}]);
